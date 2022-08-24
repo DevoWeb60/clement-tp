20a76e85519e9fb0948a6106e94f4a1d5e7eb6da
Thibault Berthelin <61558171+DevoWeb60@users.noreply.github.com>
2022-08-24 13:48:55 +0200

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Clément Construction</title>
+    @vite(['resources/sass/style.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>

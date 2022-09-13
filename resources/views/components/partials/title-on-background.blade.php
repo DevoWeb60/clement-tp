c446328da17350ec996a0133ba0021d4fe90ec61
Thibault Berthelin <61558171+DevoWeb60@users.noreply.github.com>
2022-09-13 17:03:34 +0200

    Fix pages & contact object CRUD & dynamic client page

--- resources/views/components/partials/title-on-background.blade.php
+++ resources/views/components/partials/title-on-background.blade.php
@@ -1,12 +1,12 @@
 <div class="title-on-background"
     style="background-image: linear-gradient(
     to top,
-    rgba(0,0,0, 0.8),
-    rgba(0,0,0, 0.9)
+    rgba(0,0,0, 0.7),
+    rgba(0,0,0, 0.8)
 ),
-url({{ $image }})">
+url({{ asset('storage/' . $image) }})">
     <h2 class="{{ $slot != '' ? null : 'center' }}">{{ $title }}</h2>
     @if ($slot != '')
-        <p>{{ $slot }}</p>
+        <p class="text">{{ $slot }}</p>
     @endif
 </div>
